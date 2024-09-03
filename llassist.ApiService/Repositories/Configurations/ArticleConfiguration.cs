66ec8433c4385c1d376844da3bb966b335f3c85a
Emily <emily.lomempow@gmail.com>
2024-09-03 11:33:15 +1000

    Create configurations and DB context
    Create configuration and DB Context

---
+++ llassist.ApiService/Repositories/Configurations/ArticleConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using llassist.Common.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace llassist.ApiService.Repositories.Configurations;
+public class ArticleConfiguration : IEntityTypeConfiguration<Article>
+{
+    public void Configure(EntityTypeBuilder<Article> builder)
+    {
+        builder.ToTable("Articles");
+        builder.HasKey(a => a.Id);
+        builder.OwnsOne(a => a.KeySemantics, builder =>
+        {
+            builder.ToJson();
+        });
+        builder.OwnsMany(a => a.Relevances, builder =>
+        {
+            builder.ToJson();
+        });
+        builder.HasOne(a => a.Project)
+            .WithMany(p => p.Articles)
+            .HasForeignKey(a => a.ProjectId);
+    }
+}
---
+++ llassist.ApiService/Repositories/Configurations/ProjectConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using llassist.Common.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace llassist.ApiService.Repositories.Configurations;
+
+public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+{
+    public void Configure(EntityTypeBuilder<Project> builder)
+    {
+        builder.ToTable("Projects");
+        builder.HasKey(p => p.Id);
+        builder.HasMany(p => p.Articles)
+            .WithOne(a => a.Project)
+            .HasForeignKey(a => a.ProjectId);
+        builder.OwnsOne(p => p.ResearchQuestions, builder =>
+        {
+            builder.ToJson();
+        });
+    }
+}
